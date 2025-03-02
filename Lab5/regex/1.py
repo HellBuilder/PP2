48e33928de99c1dc28e67eea8d0c14bd45d5eab9
HellBuilder <bahtybajzenis@gmail.com>
2025-03-02 20:44:39 +0500

    lab5

---
+++ Lab5/regex/1.py
@@ -0,0 +1,10 @@
+# Write a Python program that matches a string that has an 'a' followed by zero or more 'b''s.
+import re
+
+def match_a_b(string):
+    pattern = r'^a*b*$'
+    return bool(re.fullmatch(pattern, string))
+
+print(match_a_b("a")) 
+print(match_a_b("ab"))
+print(match_a_b("c"))       
